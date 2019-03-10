40214fd0685ef25c47353ee19be6fe41676f4776
rgilfanov <rgilfanov@fix.ru>
2019-03-10 23:33:31 +0200

    fixes

--- pyctogram/tests/upload_image.py
+++ pyctogram/tests/upload_image.py
@@ -6,4 +6,5 @@ if __name__ == '__main__':
     insta_client = client.InstagramClient(account.username, account.password)
     insta_client.login()
     image_path = os.path.join(os.path.dirname(__file__), 'data', 'example.jpg')
-    insta_client.upload_photo(image_path)
+    response = insta_client.upload_photo(image_path)
+    print(response)
