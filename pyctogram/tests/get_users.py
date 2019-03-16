9362ce8a55e0bf45e3be4cfa3e7d51ffa74f9307
rgilfanov <rgilfanov@fix.ru>
2019-03-16 23:53:35 +0200

    fixes

--- pyctogram/tests/get_users.py
+++ pyctogram/tests/get_users.py
@@ -3,5 +3,9 @@ from pyctogram.tests import account
 
 if __name__ == '__main__':
     victim = 'sova_timofei'
-    for user in base.get_users(account.username, account.password, victim):
+    proxies = None
+    counter = 0
+    for user in base.get_users(account.username, account.password, victim, proxies=proxies):
+        counter += 1
+        print(counter)
         print(user)
